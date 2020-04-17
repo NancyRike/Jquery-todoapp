e686929b80fd71916b95118c321fe47e24f914f9
NancyRike <nancyrike7@gmail.com>
2020-04-18 00:07:37 +0100

    mini_changes

--- todojq.js
+++ todojq.js
@@ -2,6 +2,7 @@
 var thedate = new Date();
 var arrangedate = thedate.toDateString();
 document.getElementById("todate").innerText = `Today ${arrangedate}`
+// $('input').append('<input id = "today" type = "datetime-local">')
 // code block for input date
 let today = new Date().toISOString().substr(0, 10);
 // main jQuery code block
@@ -9,10 +10,12 @@ $(document).ready(function(){
    // using the add buttton
    $('.addbutton').click(function(){
       // setting empty value
-    var output = $('#input').val();
-    $(this).val(' ');   
+   var output = $('#input').val(); 
+   $(this).val(' ');
+   var dating = $('#today').val();
+          
    //  adding li, delete button, and dynamic date
-    $('ol').append('<li>'+ output + '<input id = "today" type = "datetime-local"> <button class = "del">delete</button>')
+    $('ol').append('<li>'+ output + dating + '<button class = "del">delete</button>')
     $("li").addClass("list")
    // reseting the input field
    $('input:text').click(function(){
@@ -20,7 +23,7 @@ $(document).ready(function(){
    });
       // the delete button
      $('.del').click(function(){
-        $(this).closest('.list').remove()
+        $(this).closest('.list').fadeOut(1000)
    })
    
 });
